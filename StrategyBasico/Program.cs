948f85be8a3f7ec40564ace52d1be4ae62ba160d
Manuel Iriarte <miriarte@nisa.local>
2018-07-12 18:36:00 -0400

    creo calculadora basica

---
+++ StrategyBasico/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StrategyBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contexto = new CalculadoraContext(new Suma());
+
+            var resultado = contexto.EjecutarCalculo(2, 3);
+
+            Console.WriteLine($"el resultado es: { resultado }");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CalculadoraContext
+    {
+        ICalculoStrategy _calculoStrategy;
+
+        public CalculadoraContext(ICalculoStrategy calculoStrategy)
+        {
+            _calculoStrategy = calculoStrategy;
+        }
+
+        public int EjecutarCalculo(int numero1, int numero2)
+        {
+            return _calculoStrategy.Calcular(numero1, numero2);
+        }
+    }
+
+    public interface ICalculoStrategy
+    {
+        int Calcular(int numero1, int numero2);
+    }
+
+    public class Suma : ICalculoStrategy
+    {
+        public int Calcular(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+
+    public class Resta : ICalculoStrategy
+    {
+        public int Calcular(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+    }
+}
